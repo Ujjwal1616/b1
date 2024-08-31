66cb34c769d004797dc802c2ce32faae053f92c4
Ujjwal bhatt <UJJWAL.15524@stu.upes.ac.in>
2024-08-31 20:42:39 +0530

    basic python

---
+++ assignment.py
@@ -0,0 +1,58 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "9994160c-03b5-44e3-8722-b779abf5b8fa",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Name: UJJWAL BHATT\n",
+      "Course: MCA with cyber security\n",
+      "Python Version: 3.12.4\n"
+     ]
+    }
+   ],
+   "source": [
+    "import platform\n",
+    "name = \"UJJWAL BHATT\"\n",
+    "course = \"MCA with cyber security\"\n",
+    "print(\"Name:\", name)\n",
+    "print(\"Course:\", course)\n",
+    "print(\"Python Version:\", platform.python_version())"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fdc37869-47f2-4952-be84-fdb488683092",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
